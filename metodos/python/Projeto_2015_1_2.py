81984df36dacaac93d13bc0c65d0e3237bc8246d
Andre L. R. Didier <alrd@cin.ufpe.br>
2015-07-05 00:22:38 -0300

    Ajustes nas casas decimais do projeto 2.

--- metodos/python/Projeto_2015_1_2.py
+++ metodos/python/Projeto_2015_1_2.py
@@ -1,25 +1,25 @@
 from metodos.MetodosAjustamento import *
 from metodos.MetodosSistemasEquacoesLineares import *
 
-tabela4_1 = [
-             {'x': 1, 'f': 1.3},
-             {'x': 2, 'f': 1.8},
-             {'x': 3, 'f': 2.2},
-             {'x': 4, 'f': 0.4},
-             {'x': 5, 'f': 1.1},
-             {'x': 6, 'f': 3.0},
-             {'x': 7, 'f': 1.1},
-             {'x': 8, 'f': 0.8},
-             {'x': 9, 'f': 0.1}
-             ]
-funcoes4_1 = [ lambda x : 1, lambda x : x  ]
+#tabela4_1 = [
+#             {'x': 1, 'f': 1.3},
+#             {'x': 2, 'f': 1.8},
+#             {'x': 3, 'f': 2.2},
+#             {'x': 4, 'f': 0.4},
+#             {'x': 5, 'f': 1.1},
+#             {'x': 6, 'f': 3.0},
+#             {'x': 7, 'f': 1.1},
+#             {'x': 8, 'f': 0.8},
+#             {'x': 9, 'f': 0.1}
+#             ]
+#funcoes4_1 = [ lambda x : 1, lambda x : x  ]
 
-exemplo4_1 = MMQ(tabela4_1, funcoes4_1)
+#exemplo4_1 = MMQ(tabela4_1, funcoes4_1)
 
-exemplo4_1_SEL = LU(exemplo4_1.A, exemplo4_1.b)
+#exemplo4_1_SEL = LU(exemplo4_1.A, exemplo4_1.b)
 
-print("Exemplo 4.1")
-print(exemplo4_1_SEL.x)
+#print("Exemplo 4.1")
+#print(exemplo4_1_SEL.x)
 
 funcoes = {
            'a': math.sin,
@@ -48,10 +48,17 @@ try:
             x = eval(par_split[0])
             f = eval(par_split[1])
             tabelamento.append({"x": x, "f": f})
-        print(tabelamento)
         quantidade_funcoes = int(arquivo.readline())
         linha_funcoes = arquivo.readline() 
         letras_funcoes = map(lambda x : x.strip(), linha_funcoes.split("+"))
+        ajustamento = MMQ(tabelamento, list(map(lambda f: funcoes[f], letras_funcoes)))
+        sistema = LU(ajustamento.A, ajustamento.b)
+        x = sistema.x
+        for i,xi in enumerate(sistema.x):
+            x[i] = "{:f}".format(round(xi, 8))
+        print("Caso {}:".format(caso+1))
+        print("\t", end="")
+        print(*x, sep=", ")
 finally:
     if arquivo:
         arquivo.close()
